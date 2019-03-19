bc7658c52146018a15cd01acda2fc4ee0f6091aa
Juvenal <38957852+JuvsProgramer@users.noreply.github.com>
2019-03-18 21:09:31 -0600

    Add files via upload

---
+++ Servoconpt.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+
+Servo s1;
+int pt = A0;
+ 
+
+void setup() {
+pinMode(pt, INPUT);
+s1.attach(3);
+}
+
+void loop() {
+ 
+s1.write(map((analogRead(pt)) ,0,1023,0,180)); 
+ 
+
+
+
+
+}
